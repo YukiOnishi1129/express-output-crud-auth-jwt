ea9b750b6b5da99871396e139252ba973d70d97f
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-10-11 22:48:33 +0900

    auth service test

--- backend/src/service/auth.service.test.ts
+++ backend/src/service/auth.service.test.ts
@@ -1,6 +1,8 @@
-import { signUp } from './auth.service';
+import bcrypt from 'bcryptjs';
+import { signIn, signUp } from './auth.service';
 import { AppDataSource } from '@/config/appDataSource';
 import { User } from '@/domain/entity/user.entity';
+import { HttpError } from '@/shared/errors/httpError';
 
 describe('【Service Test Auth】', () => {
   afterEach(async () => {
@@ -26,6 +28,76 @@ describe('【Service Test Auth】', () => {
         },
       });
       expect(createdUser).toBeTruthy();
+    });
+
+    it('Fail: create new user with existing email', async () => {
+      const user = {
+        username: 'takeshi',
+        email: 'takeshi@gmail.com',
+        password: 'password',
+      };
+      const hashPassword = await bcrypt.hash(user.password, 10);
+      const userRepo = AppDataSource.getInstance().getRepository(User);
+      await userRepo.save({
+        name: user.username,
+        email: user.email,
+        password: hashPassword,
+      });
+
+      try {
+        await signUp(user);
+      } catch (error) {
+        expect(error).toEqual(
+          new HttpError(409, 'Other user already use this email'),
+        );
+      }
+    });
+  });
+
+  describe('【signIn】', () => {
+    it('Success: sign in', async () => {
+      const user = {
+        username: 'takeshi',
+        email: 'takeshi@gmail.com',
+        password: 'password',
+      };
+      const hashPassword = await bcrypt.hash(user.password, 10);
+      const userRepo = AppDataSource.getInstance().getRepository(User);
+      await userRepo.save({
+        name: user.username,
+        email: user.email,
+        password: hashPassword,
+      });
+
+      const token = await signIn({
+        email: user.email,
+        password: user.password,
+      });
+      expect(token).toBeTruthy();
+    });
+
+    it('Failed: sign in with invalid password', async () => {
+      const user = {
+        username: 'takeshi',
+        email: 'takeshi@gmail.com',
+        password: 'password',
+      };
+      const hashPassword = await bcrypt.hash(user.password, 10);
+      const userRepo = AppDataSource.getInstance().getRepository(User);
+      await userRepo.save({
+        name: user.username,
+        email: user.email,
+        password: hashPassword,
+      });
+
+      try {
+        await signIn({
+          email: user.email,
+          password: 'password1',
+        });
+      } catch (error) {
+        expect(error).toEqual(new HttpError(401, 'Invalid password'));
+      }
     });
   });
 });
